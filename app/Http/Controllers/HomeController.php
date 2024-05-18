5cc6ddba818b0592a8c601ae129c7e5078ecba3c
kylenotfound <kyleevangelisto@gmail.com>
2024-05-18 12:14:09 -0400

    more time into the void

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 
 class HomeController extends Controller {
-
     public function __invoke() {
         return view('home', [
             'posts' => Post::query()
